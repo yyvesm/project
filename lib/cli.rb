c483004a6e18722bcb7e3b6d0820705522f9117d
yves <yvesmomplais@hotmail.com>
2021-02-08 16:56:24 -0500

    adds weather_options method in cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -9,7 +9,12 @@ def prompt_for_zipcode
     puts "Enter zipcode"
     input = gets.strip
     new_location = Api.get_info(input)
-    #do sum with info
 end
+
+def weather_options(location)
+    puts "Choose from the following list for weather in #{location.name}."
+    puts "1. Temperature"
+    puts "2. Feels Like"
+    puts "3. Weather"
 
 end

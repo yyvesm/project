7235d7f3610b4d2c07fe966a7defc2ca1a681350
yves <yvesmomplais@hotmail.com>
2021-02-09 05:33:59 -0500

    adds error handling for invalid zipcode.

--- lib/api.rb
+++ lib/api.rb
@@ -2,12 +2,17 @@ class Api
 
 @@api_key = "3d33cb283c213fe0ce3114d7412d9e0f"
 
-@@url = "http://api.openweathermap.org/data/2.5/weather?zip=07093,us&units=imperial&appid=#{@@api_key}"
+
 
     
     def self.get_info(zip)
-    response = HTTParty.get(@@url)
-    binding.pry
+    url = "http://api.openweathermap.org/data/2.5/weather?zip=#{zip},us&units=imperial&appid=#{@@api_key}"
+    response = HTTParty.get(url)
+
+    if response["message"]
+        return nil
+    end
+
     weather_hash = {name: response["name"], temp: response["main"]["temp"], feels_like: response["main"]["feels_like"], weather: response["weather"][0]["description"]}
     Location.new(weather_hash)
     end
--- lib/cli.rb
+++ lib/cli.rb
@@ -9,12 +9,34 @@ def prompt_for_zipcode
     puts "Enter zipcode"
     input = gets.strip
     new_location = Api.get_info(input)
+    if new_location
+    self.weather_options(new_location)
+    else
+    puts "That zipcode is invalid."
+    self.prompt_for_zipcode
+    end
 end
 
 def weather_options(location)
-    puts "Choose from the following list for weather in #{location.name}."
+    puts "Choose from one of the numbers in the following list for weather information in #{location.name}."
     puts "1. Temperature"
     puts "2. Feels Like"
     puts "3. Weather"
+    self.user_choice(location)
+end
+
+def user_choice(location)
+    input = gets.strip
+    if input == "1"
+        puts "The temperature in #{location.name} is #{location.temp}."
+    elsif input == "2"
+        puts "In #{location.name} it feels like #{location.feels_like}."
+    elsif input == "3"
+        puts "The weather in #{location.name} is #{location.weather}."
+    else
+        puts "That is not an option."
+        self.weather_options(location)
+    end
+end
 
 end
